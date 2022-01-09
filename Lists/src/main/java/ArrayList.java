8638e3ce39c8af5bbbaba946cfdd4804851ac639
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2022-01-09 23:10:54 +0200

    Update ArrayList.java

--- Lists/src/main/java/ArrayList.java
+++ Lists/src/main/java/ArrayList.java
@@ -1,3 +1,5 @@
+
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
@@ -13,98 +15,237 @@ public class ArrayList<E> implements List<E> {
         this.elements = new Object[size];
     }
 
+    //CREATE
+
     @Override
     public boolean add(E element) {
-        return false;
+        grow();
+        this.elements[this.size++] = element;
+
+        return true;
+    }
+
+    @Override
+    public boolean add(int index, E element) {
+        if (index < 0 || index > this.size) {
+            throw new IllegalStateException();
+        }
+        grow();
+
+        shiftRight(index);
+        elements[index] = element;
+
+
+        return true;
     }
 
     @Override
     public boolean addFirst(E element) {
-        return false;
+        return add(0, element);
     }
 
     @Override
     public boolean addLast(E element) {
-        return false;
+        return add(element);
     }
+
+    //RETRIEVE
 
     @Override
     public E get(int index) {
-        return null;
+        checkIndex(index);
+
+        return (E) this.elements[index];
     }
 
     @Override
     public int indexOf(E element) {
-        return 0;
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(E element) {
-        return 0;
+        for (int i = this.size - 1; i >= 0; i--) {
+            if (this.elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return Arrays.copyOf(this.elements, this.size);
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return this.index < size;
+            }
+
+            @Override
+            public E next() {
+                return (E) elements[index++];
+            }
+        };
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean contains(Object o) {
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(o)) {
+                return true;
+            }
+        }
         return false;
     }
 
+    //UPDATE
+
     @Override
     public E set(int index, E element) {
-        return null;
+        checkIndex(index);
+        E e = (E) this.elements[index];
+        this.elements[index] = element;
+        return e;
     }
 
     @Override
     public boolean replace(E oldElement, E newElement) {
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(oldElement)) {
+                this.elements[i] = newElement;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean replaceAll(E oldElement, E newElement) {
-        return false;
+        boolean flag = false;
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(oldElement)) {
+                this.elements[i] = newElement;
+                flag = true;
+            }
+        }
+        return flag;
     }
 
-    @Override
-    public boolean swap(int index1, int index2) {
-        return false;
-    }
+    //DELETE
 
     @Override
     public E remove(int index) {
-        return null;
+        checkIndex(index);
+        E element = (E) this.elements[index];
+        shiftLeft(index);
+
+        shrink();
+        return element;
     }
 
     @Override
     public boolean remove(Object element) {
+        for (int i = 0; i < this.size; i++) {
+            if (elements[i].equals(element)) {
+                shiftLeft(i);
+                shrink();
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
+    public E removeFirst() {
+        if (isEmpty()) {
+            throw new IllegalStateException();
+        }
+        E element = (E) this.elements[0];
+        shiftLeft(0);
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        if (isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        E element = (E) this.elements[this.size - 1];
+        shiftLeft(this.size - 1);
+        return element;
+    }
+
+    @Override
     public void clear() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
 
     }
 
     @Override
     public boolean removeAll(E element) {
-        return false;
+        boolean flag = false;
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(element)) {
+                shiftLeft(i);
+                flag = true;
+            }
+        }
+        shrink();
+        return flag;
     }
+
+    //HELPERS
+    private void grow() {
+        if (this.size == this.elements.length) {
+            this.elements = Arrays.copyOf(this.elements, this.elements.length * 2);
+        }
+    }
+
+    private void shrink() {
+        if (this.size <= this.elements.length / 4) {
+            this.elements = Arrays.copyOf(this.elements, this.elements.length / 2);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException("IndexOutBounds");
+        }
+    }
+
+    private void shiftRight(int index) {
+        System.arraycopy(elements, index, elements, index + 1, this.size - index);
+        this.size++;
+    }
+
+    private void shiftLeft(int index) {
+        System.arraycopy(this.elements, index + 1, this.elements, index, this.size - index);
+        this.size--;
+    }
+
+
 }
