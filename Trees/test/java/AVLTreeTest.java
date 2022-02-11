44aef97e97f95afaa27439a354a3dc98d1d36e2f
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2022-02-11 23:06:02 +0200

    Update AVLTreeTest.java

--- Trees/test/java/AVLTreeTest.java
+++ Trees/test/java/AVLTreeTest.java
@@ -1,38 +1,41 @@
-import junit.framework.TestCase;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 public class AVLTreeTest {
-    private static final Integer[] PREORDER = new Integer[]{25, 15, 10, 4, 12, 22, 18, 24, 50, 35, 31, 44, 70, 66, 90};
-    private static final Integer[] INORDER = new Integer[]{4, 10, 12, 15, 18, 22, 24, 25, 31, 35, 44, 50, 66, 70, 90};
-    private static final Integer[] POSTORDER = new Integer[]{4, 12, 10, 18, 24, 22, 15, 31, 44, 35, 66, 90, 70, 50, 25};
-    private static final Integer[] LEVELORDER = new Integer[]{25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90};
+    private static final Integer[] INSERTORDER = new Integer[]{60, 16, 70, 90, 77, 38, 96, 84, 50, 15, 30, 82, 91, 10, 54, 20};
+    private static final Integer[] PREORDER = new Integer[]{60, 16, 15, 10, 38, 30, 20, 50, 54, 84, 77, 70, 82, 91, 90, 96};
+    private static final Integer[] INORDER = new Integer[]{10, 15, 16, 20, 30, 38, 50, 54, 60, 70, 77, 82, 84, 90, 91, 96};
+    private static final Integer[] POSTORDER = new Integer[]{10, 15, 20, 30, 54, 50, 38, 16, 70, 82, 77, 90, 96, 91, 84, 60};
+    private static final Integer[] LEVELORDER = new Integer[]{60, 16, 84, 15, 38, 77, 91, 10, 30, 50, 70, 82, 90, 96, 20, 54};
 
-    private BinaryTree<Integer> createBinaryTree() {
-        BinaryTree<Integer> binaryTree = new BinaryTree<>();
-        for (Integer integer : PREORDER) {
-            binaryTree.insert(integer);
+    private AVLTree<Integer> createAVLTree() {
+        AVLTree<Integer> avlTree = new AVLTree<>();
+        for (Integer integer : INSERTORDER) {
+            avlTree.insert(integer);
         }
-        return binaryTree;
+        return avlTree;
 
     }
 
 
     @Test
     public void testContains() {
-        BinaryTree<Integer> binaryTree = createBinaryTree();
+        AVLTree<Integer> avlTree = createAVLTree();
         for (Integer integer : PREORDER) {
-            Assert.assertTrue(binaryTree.contains(integer));
+            Assert.assertTrue(avlTree.contains(integer));
         }
-        Assert.assertFalse(binaryTree.contains(-1));
+        Assert.assertFalse(avlTree.contains(-1));
     }
 
     @Test
     public void testPreOrder() {
-        BinaryTree<Integer> binaryTree = createBinaryTree();
-        List<Integer> list = (List<Integer>) binaryTree.preOrder();
+        AVLTree<Integer> avlTree = createAVLTree();
+        List<Integer> list = (List<Integer>) avlTree.preOrder();
+
         Assert.assertEquals(PREORDER.length, list.size());
 
         for (int i = 0; i < list.size(); i++) {
@@ -43,8 +46,9 @@ public class AVLTreeTest {
 
     @Test
     public void testInOrder() {
-        BinaryTree<Integer> binaryTree = createBinaryTree();
-        List<Integer> list = (List<Integer>) binaryTree.inOrder();
+        AVLTree<Integer> avlTree = createAVLTree();
+        List<Integer> list = (List<Integer>) avlTree.inOrder();
+
         Assert.assertEquals(INORDER.length, list.size());
 
         for (int i = 0; i < list.size(); i++) {
@@ -54,10 +58,10 @@ public class AVLTreeTest {
 
     @Test
     public void testPostOrder() {
-        BinaryTree<Integer> binaryTree = createBinaryTree();
-        List<Integer> list = (List<Integer>) binaryTree.postOrder();
-        Assert.assertEquals(POSTORDER.length, list.size());
+        AVLTree<Integer> avlTree = createAVLTree();
+        List<Integer> list = (List<Integer>) avlTree.postOrder();
 
+        Assert.assertEquals(POSTORDER.length, list.size());
         for (int i = 0; i < list.size(); i++) {
             Assert.assertEquals(POSTORDER[i], list.get(i));
         }
@@ -65,12 +69,27 @@ public class AVLTreeTest {
 
     @Test
     public void testLevelOrder() {
-        BinaryTree<Integer> binaryTree = createBinaryTree();
-        List<Integer> list = (List<Integer>) binaryTree.levelOrder();
+        AVLTree<Integer> avlTree = createAVLTree();
+        List<Integer> list = (List<Integer>) avlTree.levelOrder();
+
         Assert.assertEquals(LEVELORDER.length, list.size());
         for (int i = 0; i < list.size(); i++) {
             Assert.assertEquals(LEVELORDER[i], list.get(i));
         }
+    }
+
+    @Test
+    public void testGetMin() {
+        AVLTree<Integer> avlTree = createAVLTree();
+        int min = avlTree.minValue();
+        Assert.assertEquals(10, min);
+    }
+
+    @Test
+    public void testGetMax() {
+        AVLTree<Integer> avlTree = createAVLTree();
+        int max = avlTree.maxValue();
+        Assert.assertEquals(96, max);
     }
 
 
